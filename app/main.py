c9c5f917c4e91675e52efd831b909bdb1f24e18d
jonathan-imanu <jonathan.imanuel@gmail.com>
2024-03-29 21:57:17 -0400

    Respond to HTTP Request

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 import socket
 
+CRLF = "\r\n"
 
 def main():
     print("Logs from your program will appear here!")
 
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() 
+    
+    while True:
+        conn, addr = server_socket.accept() 
+        conn.sendall("HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
