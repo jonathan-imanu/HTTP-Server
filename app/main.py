14ae12cb46483fcb8f24c04bb31fc05501c35fe9
jonathan-imanu <jonathan.imanuel@gmail.com>
2024-03-29 21:58:18 -0400

    Respond to HTTP Request

--- app/main.py
+++ app/main.py
@@ -8,9 +8,9 @@ def main():
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     
-    while True:
-        conn, addr = server_socket.accept() 
-        conn.sendall("HTTP/1.1 200 OK\r\n\r\n")
+    conn, addr = server_socket.accept() 
+    conn.sendall("HTTP/1.1 200 OK\r\n\r\n")
+    server_socket.close()
 
 
 if __name__ == "__main__":
