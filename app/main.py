6214c35f7af4dc361b92c248ca9732734ebcae3b
jonathan-imanu <jonathan.imanuel@gmail.com>
2024-03-29 22:09:38 -0400

    Added Server Class

--- app/main.py
+++ app/main.py
@@ -2,6 +2,18 @@ import socket
 
 CRLF = "\r\n"
 
+class Server():
+    def __init__(self):
+        self.server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    
+    def run(self):
+        conn, addr = self.server_socket.accept() 
+        while True:
+            conn.sendall("HTTP/1.1 200 OK\r\n\r\n".encode())
+            while req := conn.recv(1024):
+                data = req.decode().split(CRLF)
+        
+        
 def main():
     
     """
@@ -12,17 +24,8 @@ Host: localhost:4221
 User-Agent: curl/7.64.1
 
     """
-    
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    
-    conn, addr = server_socket.accept() 
-    while True:
-        conn.sendall("HTTP/1.1 200 OK\r\n\r\n".encode())
-        while req := conn.recv(1024):
-            data = req.decode().split(CRLF)
-            print(data)
-            
-        
+    server = Server
+    server.run()
         
 
 if __name__ == "__main__":
