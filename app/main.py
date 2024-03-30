63b5ee9d59146451e547b40b6f584ed8ed5c5543
jonathan-imanu <jonathan.imanuel@gmail.com>
2024-03-29 22:14:21 -0400

    Added Server class

--- app/main.py
+++ app/main.py
@@ -2,11 +2,22 @@ import socket
 
 CRLF = "\r\n"
 
+"""
+HTTP Request Contents
+    
+GET /index.html HTTP/1.1
+Host: localhost:4221
+User-Agent: curl/7.64.1
+
+"""
+
 class Server():
-    def __init__(self):
-        self.server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    # A server class to handle and respond to HTTP requests
+    def __init__(self, port):
+        self.port = port
     
     def run(self):
+        server_socket = socket.create_server(("localhost", self.port), reuse_port=True)
         conn, addr = self.server_socket.accept() 
         while True:
             conn.sendall("HTTP/1.1 200 OK\r\n\r\n".encode())
@@ -16,15 +27,8 @@ class Server():
         
 def main():
     
-    """
-    HTTP Request Contents
-    
-GET /index.html HTTP/1.1
-Host: localhost:4221
-User-Agent: curl/7.64.1
-
-    """
-    server = Server
+   
+    server = Server(4221)
     server.run()
         
 
